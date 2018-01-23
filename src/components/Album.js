73181c852a29d46dfe03ad035c6d440b87503068
Jacob <jake.laurenti@gmail.com>
2018-01-22 19:43:03 -0500

    Audio Playback working

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,8 +10,39 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }   
+      this.play();
+    }
   }
 
   render() {
@@ -34,7 +65,7 @@ class Album extends Component {
           <tbody>
            {
             this.state.album.songs.map((song, index) =>
-              <tr>
+            <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
                 <td>
                   <button>
                    <span className="song-number">{ index + 1 }</span>
